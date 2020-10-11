a994c9f6f62f07516c88717db9e68ed74876c456
Andrew <andrew.berry91@gmail.com>
2020-10-11 16:05:40 -0500

    Add console writes to get rid of warnings in Helpers.cs

--- Utils/Helpers.cs
+++ Utils/Helpers.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Web;
 using System.IO;
 using Amazon;
 using Amazon.SecretsManager;
@@ -60,36 +56,42 @@ namespace AWSSecrets
             {
                 // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
                 // Deal with the exception here, and/or rethrow at your discretion.
+                Console.WriteLine(e.Message);
                 throw;
             }
             catch (InternalServiceErrorException e)
             {
                 // An error occurred on the server side.
                 // Deal with the exception here, and/or rethrow at your discretion.
+                Console.WriteLine(e.Message);
                 throw;
             }
             catch (InvalidParameterException e)
             {
                 // You provided an invalid value for a parameter.
                 // Deal with the exception here, and/or rethrow at your discretion
+                Console.WriteLine(e.Message);
                 throw;
             }
             catch (InvalidRequestException e)
             {
                 // You provided a parameter value that is not valid for the current state of the resource.
                 // Deal with the exception here, and/or rethrow at your discretion.
+                Console.WriteLine(e.Message);
                 throw;
             }
             catch (ResourceNotFoundException e)
             {
                 // We can't find the resource that you asked for.
                 // Deal with the exception here, and/or rethrow at your discretion.
+                Console.WriteLine(e.Message);
                 throw;
             }
             catch (System.AggregateException ae)
             {
                 // More than one of the above exceptions were triggered.
                 // Deal with the exception here, and/or rethrow at your discretion.
+                Console.WriteLine(ae.Message);
                 throw;
             }
 
